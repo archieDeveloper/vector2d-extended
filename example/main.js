e50c92e8145a93b297cc26e0f591e24e5bcba86e
archieDeveloper <arkadij.ok@gmail.com>
2015-11-12 18:24:13 +0700

    Сделал более хорошие и интерактивные примеры

--- example/main.js
+++ example/main.js
@@ -143,59 +143,58 @@ var render = function() {
 }
 
 Mouse = (function() {
-var instance, mouseDown, mousePress, mouseUp;
+  var instance, mouseDown, mousePress, mouseUp;
 
-mousePress = {};
+  mousePress = {};
 
-mouseDown = {};
+  mouseDown = {};
 
-mouseUp = {};
+  mouseUp = {};
 
-instance = null;
+  instance = null;
 
-Mouse.prototype.position = new Vector2d(0, 0);
+  Mouse.prototype.position = new Vector2d(0, 0);
 
-function Mouse() {
-  canvas.addEventListener('mousemove', (function(_this) {
-    return function(e) {
-      gameLoop();
-      _this.position.x = e.offsetX == null ? e.layerX : e.offsetX;
-      return _this.position.y = e.offsetY == null ? e.layerY : e.offsetY;
+  function Mouse() {
+    canvas.addEventListener('mousemove', (function(_this) {
+      return function(e) {
+        gameLoop();
+        _this.position.x = e.offsetX == null ? e.layerX : e.offsetX;
+        return _this.position.y = e.offsetY == null ? e.layerY : e.offsetY;
+      };
+    })(this));
+    canvas.addEventListener('mouseDown', function(e) {
+      mousePress[e.which] = true;
+      return mouseDown[e.which] = true;
+    });
+    canvas.addEventListener('mouseUp', function(e) {
+      delete mousePress[e.which];
+      return mouseUp[e.which] = true;
+    });
+    canvas.oncontextmenu = function() {
+      return false;
     };
-  })(this));
-  canvas.addEventListener('mouseDown', function(e) {
-    mousePress[e.which] = true;
-    return mouseDown[e.which] = true;
-  });
-  canvas.addEventListener('mouseUp', function(e) {
-    delete mousePress[e.which];
-    return mouseUp[e.which] = true;
-  });
-  canvas.oncontextmenu = function() {
-    return false;
-  };
-}
-
-Mouse.getInstance = function() {
-  if (instance == null) {
-    instance = new Mouse;
   }
-  return instance;
-};
 
-Mouse.prototype.isPressed = function(code) {
-  return mousePress[code] != null;
-};
+  Mouse.getInstance = function() {
+    if (instance == null) {
+      instance = new Mouse;
+    }
+    return instance;
+  };
+  Mouse.prototype.isPressed = function(code) {
+    return mousePress[code] != null;
+  };
 
-Mouse.prototype.isDown = function(code) {
-  return mouseDown[code] != null;
-};
+  Mouse.prototype.isDown = function(code) {
+    return mouseDown[code] != null;
+  };
 
-Mouse.prototype.isUp = function(code) {
-  return mouseUp[code] != null;
-};
+  Mouse.prototype.isUp = function(code) {
+    return mouseUp[code] != null;
+  };
 
-return Mouse;
+  return Mouse;
 
 })();
 
